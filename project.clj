c0bbb2cd15fbf3276c67c79384f7994ca647767f
Brad Lucas <brad@beaconhill.com>
2016-10-15 21:45:29 -0400

    version 1.8.0 of clojure

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cipher "1.0.0-SNAPSHOT"
+(defproject cipher "1.0.0"
   :description "AI Class cipher problem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot cipher.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
